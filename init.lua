f4ba2793d0e0097d7e95190b8e36e43c92cd95f2
Alexander Sandukovskiy <asandukovskiy@gmail.com>
2024-01-02 16:02:37 +0300

    Create my config LazyVim

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+
+vim.g.python3_host_prog = os.getenv("NEOVIM_PYTHON_EXEC")
 require("config.lazy")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,33 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Shorten function name
+local keymap = vim.keymap.set
+local cmd = vim.cmd
+-- Silent keymap option
+local opts = { silent = true, noremap = true }
+
+-- Expand %% to current path
+cmd("cnoremap <expr> %% getcmdtype() == ':' ? expand('%:h').'/' : '%%'")
+
+keymap("n", "Q", "<cmd>bdelete<CR>", opts)
+
+function getVisualSelection()
+  vim.cmd('noau normal! "vy"')
+  local text = vim.fn.getreg("v")
+  vim.fn.setreg("v", {})
+
+  text = string.gsub(text, "\n", "")
+  if #text > 0 then
+    return text
+  else
+    return ""
+  end
+end
+
+keymap("v", "<leader>ft", function()
+  require("telescope.builtin").live_grep({
+    default_text = getVisualSelection(),
+  })
+end, opts)
